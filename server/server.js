418bb14f23b6737cfcfede93fdf113b9d8eb174f
S Banerjee <77569575+Agent-Shu@users.noreply.github.com>
2024-12-16 01:00:42 +0530

    Basic Home Layout
    Made a very simple landing page just to test out how things gonna look like and what to adjust and what not to.
    
    Buttons and other necessary things will be added later.

---
+++ server/server.js
@@ -0,0 +1,58 @@
+require("dotenv").config({ path: "./server/secrets.env" });
+
+const https = require("https");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+const app = express();
+const path = require("path");
+const port = 80;
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+app.get("/", async (req, res) => {
+    res.sendFile(path.join(__dirname, "/../public/home/home.html"));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+  });
+  
+
+
+module.exports = app;
+
+
+
+
