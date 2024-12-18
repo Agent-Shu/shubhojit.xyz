cae26be2e5582bfc67903447c3a8a25d2330585e
S Banerjee <77569575+Agent-Shu@users.noreply.github.com>
2024-12-19 01:04:02 +0530

    Added Background Moving thingies
    Added background png's and gonna use vercel for now as cdn for the images.
    
    Later will be moving it to imagekit or amazon cuz why not /shrug
    
    Also, F*king hell. Took some time cuz me poo poo in css : (

--- public/home/home.js
+++ public/home/home.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const movingElement = document.querySelector('.hompage_landing');
-    const movementStrength = 40; 
+    const movementStrength = 35; 
     document.addEventListener('mousemove', (event) => {
         const { clientX, clientY } = event; 
         const centerX = window.innerWidth / 2; 
@@ -13,8 +12,42 @@ document.addEventListener("DOMContentLoaded", () => {
         movingElement.style.transform = `translate(${moveX}px, ${moveY}px)`;
     });
 
+    const pic1 = document.querySelector('.pic_1');
+    document.addEventListener('mousemove', (event) => {
+        const screenWidth = window.innerWidth;
+        const hue = (event.clientX / screenWidth) * 360;
+        pic1.style.filter = `hue-rotate(${hue}deg)`;
+    });
+
+    const pic2 = document.querySelector('.pic_2');
+    document.addEventListener('mousemove', (event) => {
+        const screenWidth = window.innerWidth;
+        const hue = (event.clientX / screenWidth) * 360;
+        pic2.style.filter = `hue-rotate(${hue+20}deg)`;
+    });
 
 
+    const pic_1 = document.querySelector('.pic_1');
+    document.addEventListener('mousemove', (event) => {
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+        const xPercent = event.clientX / screenWidth;
+        const yPercent = event.clientY / screenHeight;
+        const skewX = (xPercent - 0.5) * 35; 
+        const skewY = (yPercent - 0.5) * 30; 
+        pic_1.style.transform = `skew(${skewX}deg, ${skewY}deg)`;
+    });
+
+    const pic_2 = document.querySelector('.pic_2');
+    document.addEventListener('mousemove', (event) => {
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+        const xPercent = event.clientX / screenWidth;
+        const yPercent = event.clientY / screenHeight;
+        const skewX = (xPercent - 0.5) * 22; 
+        const skewY = (yPercent - 0.5) * 30; 
+        pic_2.style.transform = `skew(${skewX}deg, ${skewY}deg)`;
+    });
 
 
 
